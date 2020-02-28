8f8ebd4b26aca83ccc6a047977f10054eb8ac988
jetreal <41300107+jetreal@users.noreply.github.com>
2020-02-28 20:44:41 +0300

    Updates

---
+++ precache-manifest.480cfa2251dcfa23583aa65d91168b12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec882d10dc9f6f508d3c77a0d86967f9",
+    "url": "/paymentsystem/index.html"
+  },
+  {
+    "revision": "4d5ae1ae1fedcee508a7",
+    "url": "/paymentsystem/static/css/main.afb3b4c2.chunk.css"
+  },
+  {
+    "revision": "b423ca20beed56785810",
+    "url": "/paymentsystem/static/js/2.bd356e1f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/paymentsystem/static/js/2.bd356e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5ae1ae1fedcee508a7",
+    "url": "/paymentsystem/static/js/main.b1d0a4ab.chunk.js"
+  },
+  {
+    "revision": "fde4d63f63c228f1216f",
+    "url": "/paymentsystem/static/js/runtime-main.930bbfa0.js"
+  }
+]);
